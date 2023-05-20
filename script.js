dc54b4fe05a46204cb896d2a2a6dfa0eaa5e95b8
Sheril <sherilmariamjose@gmail.com>
2023-05-20 13:20:15 +0530

    animate

--- script.js
+++ script.js
@@ -28,4 +28,30 @@ window.onscroll=() =>{
     /*sticky navbar */
     let header=document.querySelectorAll('header');
     header.classList.toggle('sticky',window.scrollY > 100);
+
+/*remove toggle when click navbar link */
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
 };
+/*scroll reveal */
+ScrollReveal
+({
+     reset: true ,
+     distance:'80px',
+     duration:2000,
+     delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading' ,{ origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container,.portfolio-box,.contact form' ,{ origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img' ,{ origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content' ,{ origin:'right'});
+
+/*typed js*/ 
+const typed =new Typed('.multiple-text',{
+    strings:['Frontend Developer','Public Speaker','Polyglot','Music Lover'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
